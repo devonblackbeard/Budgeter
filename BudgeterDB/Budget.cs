a4e2db99c8b2cbc9fb5972ca96c55149f5c1b547
Devon Blackbeard <32315720+devonblackbeard@users.noreply.github.com>
2021-10-30 18:02:28 -0600

    remove comment

--- BudgeterDB/Budget.cs
+++ BudgeterDB/Budget.cs
@@ -6,7 +6,7 @@ namespace BudgeterDB
     public class Budget
     {
         [Key]
-        public int Id { get; set; } //pk
+        public int Id { get; set; }
         public double Amount { get; set; }
         public string Description { get; set; }
           
